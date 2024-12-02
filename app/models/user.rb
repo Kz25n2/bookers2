027d2f4f7673741af9ce2d63258b17b3071f5a8f
EC2 Default User <ec2-user@ip-172-31-46-192.eu-north-1.compute.internal>
2024-12-02 08:04:47 +0000

    [Add]フォロー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,23 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  has_many :relationships, foreign_key: :following_id
+  has_many :followings, through: :relationships, source: :follower
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :reverse_of_relationships, source: :following
+
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def is_followed_by?(user)
+    reverse_of_relationships.find_by(following_id: user.id).present?
   end
 
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
